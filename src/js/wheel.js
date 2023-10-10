f358b3607e3c51364d8f5050ad62e1e5eb610d19
seoLeir <75039620+seoLeir@users.noreply.github.com>
2023-10-10 18:56:15 +0500

    Update wheel.js

---
+++ src/js/wheel.js
@@ -0,0 +1,37 @@
+ymaps.ready(function () {
+    ymaps.option.Manager.set({
+        // Выбор светлой или темной темы в зависимости от системных настроек
+        theme: window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    });
+
+    // Получение текущей локации пользователя
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            // Инициализация карты с полученными координатами
+            initMap([latitude, longitude]);
+        });
+    } else {
+        // Если геолокация недоступна, инициализируйте карту с координатами по умолчанию
+        initMap([55.76, 37.64]);
+    }
+});
+
+var myMap, myPlacemark;
+
+function initMap(centerCoordinates) {
+    myMap = new ymaps.Map("map", {
+        center: centerCoordinates,
+        zoom: 7,
+        controls: ['geolocationControl', 'fullscreenControl', 'zoomControl'] // Включение зум-контрола
+    });
+
+    myPlacemark = new ymaps.Placemark(centerCoordinates, {
+        hintContent: 'Ваше местоположение',
+        balloonContent: 'Вы здесь'
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+}
